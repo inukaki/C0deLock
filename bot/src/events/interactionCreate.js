9991a7ca57f1a683ec1f08cfc89eaee57d5ac759
inukaki <inukaki8@gmail.com>
2025-04-17 23:14:44 +0900

    不要な行を削除削除

--- bot/src/events/interactionCreate.js
+++ bot/src/events/interactionCreate.js
@@ -40,37 +40,6 @@ module.exports = {
                 console.error(error);
                 await interaction.reply('コマンドの実行中にエラーが発生しました。');
             }
-
-            // // registerコマンドが実行されたときに実行される処理
-            // if(interaction.commandName === registerFile.data.name) {
-
-            //     try {
-            //         // register.jsのexecute関数を実行
-            //         console.log('registerコマンドが実行されました。');
-            //         await registerFile.execute(interaction);
-            //     } catch (error) {
-            //         console.error(error);
-            //         await interaction.reply('コマンドの実行中にエラーが発生しました。');
-            //     }
-
-            // }
-            // // unregisterコマンドが実行されたときに実行される処理
-            // else if(interaction.commandName === unregisterFile.data.name){
-
-            //     try{
-            //         console.log('unregisterコマンドが実行されました。');
-            //         await unregisterFile.execute(interaction);
-            //     } catch (error) {
-            //         console.error(error);
-            //         await interaction.reply('コマンドの実行中にエラーが発生しました。');
-            //     }
-
-            // }
-            
-            
-            // else{
-            //     await interaction.reply('そのコマンドは存在しません。');
-            // }
         });  
     }
 }
